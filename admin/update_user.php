138556c306e9b96a21396ded79a4081a9f9dacd9
ReyCannavaro <reyjunoalcannavaro@gmail.com>
2024-04-04 13:06:09 +0700

    pesan commit

--- admin/update_user.php
+++ admin/update_user.php
@@ -3,17 +3,18 @@ include '../koneksi.php';
 
 if(isset($_POST['update'])) {
     $id = $_POST['id'];
+    $nama = $_POST['nama'];
     $username = $_POST['username'];
     $password = $_POST['password'];
     $domisili = $_POST['domisili'];
 
     // Lakukan proses update data di database
-    $query = "UPDATE user SET username='$username', password='$password', domisili='$domisili' WHERE id=$id";
+    $query = "UPDATE user SET nama='$nama', username='$username', password='$password', domisili='$domisili' WHERE id=$id";
     $result = mysqli_query($mysqli, $query);
 
     if($result) {
         echo "Data berhasil diperbarui.";
-        header("Location: user.php"); // Redirect kembali ke halaman data user
+        header("Location: users.php"); // Redirect kembali ke halaman data user
         exit();
     } else {
         echo "Terjadi kesalahan: " . mysqli_error($mysqli);
@@ -38,16 +39,19 @@ if(isset($_GET['id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update User</title>
     <link rel="icon" type="image/png" href="img/logotitle.png">
-    <link rel="stylesheet" href="styleproduk.css">
+    <link rel="stylesheet" href="styleupdate.css">
 </head>
 <body>
 <div class="container">
         <header>
-            <h1 class="title">Tambah Produk</h1>
+            <h1 class="title">Update User</h1>
         </header>
         <section class="form">
         <form method="POST" action="">
         <input type="hidden" name="id" value="<?php echo $data['id']; ?>">
+
+        <label for="username">Nama</label><br>
+        <input type="text" id="nama" name="nama" value="<?php echo $data['nama']; ?>"><br>
 
         <label for="username">Username:</label><br>
         <input type="text" id="username" name="username" value="<?php echo $data['username']; ?>"><br>
@@ -56,7 +60,7 @@ if(isset($_GET['id'])) {
         <input type="password" id="password" name="password" value="<?php echo $data['password']; ?>"><br>
 
         <label for="domisili">domisili:</label><br>
-        <input type="text" id="domisili" name="domisili" value="<?php echo $data['domisili']; ?>"><br><br>
+        <input type="domisili" id="domisili" name="domisili" value="<?php echo $data['domisili']; ?>"><br><br>
         <input type="submit" name="update" value="Update" class="button">
     </form>
         </section>
--- koneksi.php
+++ koneksi.php
@@ -7,7 +7,7 @@ $databasepassword = "";
 $mysqli = mysqli_connect($databasehost, $databaseusername, $databasepassword, $databasename);
 
 if ($mysqli){
-    echo "koneksi db berhasil.<br/>";
+    //echo "koneksi db berhasil.<br/>";
 }else{
     echo "koneksi db gagal.<br/>";
 }
